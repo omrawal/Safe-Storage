587b8cf88dc3697c911688255fb8d768275f91ee
Om Rawal <omrawal007@gmail.com>
2021-03-19 10:43:11 +0530

    Models.py attempt

--- safestorage/storage_app/models.py
+++ safestorage/storage_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
@@ -8,4 +9,13 @@ class User(models.Model):
     email = models.EmailField(max_length=254, null=True, blank=True)
     four_digit_pass = models.CharField(max_length=256, null=True, blank=True)
     private_key = models.CharField(max_length=256, null=True, blank=True)
-    profile_pic = models.ImageField(upload_to='static/profilepic')
+    auth_image = models.ImageField(
+        upload_to='static/auth_image/%s' % username, null=True)
+    profile_pic = models.ImageField(upload_to='static/profilepic', null=True)
+
+
+class Encrypted_files(models.Model):
+    owner = models.ForeignKey('User', null=True)
+    encrypted_file = models.FileField(
+        upload_to='static/encrypted_files', null=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True, null=True)
