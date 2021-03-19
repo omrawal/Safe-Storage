728c5be99e481d78b623081847a12eb031c57ebf
Om Rawal <omrawal007@gmail.com>
2021-03-19 15:29:50 +0530

    Registration Working Correctly

---
+++ safestorage/storage_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import fields
+from .models import *
+
+
+class RegistrationForm(forms.ModelForm):
+    four_digit_pass = forms.CharField(widget=forms.PasswordInput())
+    # private_key = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User_data
+        fields = ['username', 'email', 'four_digit_pass',
+                  'profile_pic', 'private_key']
--- safestorage/storage_app/views.py
+++ safestorage/storage_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.forms.widgets import HiddenInput
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import User_data, Encrypted_files
 from .file_crypto import *
+from .forms import *
 
 # Create your views here.
 
@@ -9,15 +12,31 @@ def home(request):
     return render(request, 'storage_app/home.html',)
 
 
-def register(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        four_digit_pass = request.POST.get('password')
-        private_key = generateKey()
-        print(username)
-        print(email)
-        print(four_digit_pass)
-        print(private_key)
+# def register(request):
+#     if request.method == "POST":
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         four_digit_pass = request.POST.get('password')
+#         private_key = generateKey()
+#         profile_pic = request.FILES.get('profile_pic')
+#         print(username)
+#         print(email)
+#         print(four_digit_pass)
+#         print(private_key)
+#         print(profile_pic)
+#         print(type(profile_pic))
 
-    return render(request, 'storage_app/register.html',)
+#     return render(request, 'storage_app/register.html',)
+
+
+def register(request):
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('storage-home')
+    else:
+        form = RegistrationForm(
+            initial={'private_key': generateKey()})
+    return render(request, 'storage_app/register.html', {'form': form})
