cdf0abe308ab8bed910086aa6ec9d38a835ee5a4
Om Rawal <omrawal007@gmail.com>
2021-03-20 08:32:18 +0530

    Shift from Django to tkinter

--- safestorage/storage_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User_data, Encrypted_files
-# Register your models here.
-admin.site.register(User_data)
-admin.site.register(Encrypted_files)
-# admin.site.register(auth_Image)
--- safestorage/storage_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StorageAppConfig(AppConfig):
-    name = 'storage_app'
